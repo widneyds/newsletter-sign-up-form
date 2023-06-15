a290e77bf184eaa5d70e3a2e7a7b4c2eda51eb5e
widneyds <widneyds_dev@proton.me>
2023-06-15 18:04:26 -0300

    initial full version completed (with javascript)

---
+++ app.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('.c-form')
+const userEmailContainer = document.querySelector('.user-email-container')
+const emailTitleError = document.querySelector('.u-email-title--error')
+const successMessage = document.querySelector('.success-message')
+const dismissMessage = document.querySelector('.dismiss-message')
+
+let allowShowSuccessMessage = false
+
+form.email.addEventListener('input', event => {
+    const input = event.target
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    const isAValidEmail = emailRegex.test(input.value)
+
+    if (isAValidEmail) {
+        input.classList.remove('u-input--error')
+        emailTitleError.style.display = 'none'
+        allowShowSuccessMessage = true
+    } else {
+        input.classList.add('u-input--error')
+        emailTitleError.style.display = 'inline'
+    }
+})
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const userEmail = event.target.email.value
+
+    userEmailContainer.textContent = userEmail
+
+    if (allowShowSuccessMessage) {
+        successMessage.style.display = 'flex'
+    }
+})
+
+dismissMessage.addEventListener('click', () => {
+    successMessage.style.display = 'none'
+})
